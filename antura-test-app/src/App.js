32ef8d90a5510e25fe27023a8007dd13bf92b6b0
Almir Kapa <almir.kapa@gmail.com>
2020-10-29 22:41:11 +0100

    Basic fetch user functionality with loading and error handling

--- antura-test-app/src/App.js
+++ antura-test-app/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Container from '@material-ui/core/Container';
+import Button from "@material-ui/core/Button";
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = "https://randomuser.me/api/"
+
+class App extends React.Component {
+  state = {
+    fetchedData: {
+      results: [],
+      info: {}
+    },
+    isLoading: true,
+    error: null
+  }
+
+  componentDidMount() {
+    this.fetchUser()
+  }
+
+  fetchUser() {
+    console.log("fetching user....")
+
+    fetch(API_URL)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw Error("An occurred while fetching the user")
+        }
+      })
+      .then(res => this.setState({ fetchedData: res, isLoading: false }))
+      .catch(error => this.setState({ error }))
+  }
+
+  render() {
+    const { fetchedData, isLoading, error } = this.state
+
+    return (
+      <Container className="App" maxWidth="sm">
+        <Button variant="outlined" color="primary" disabled={isLoading} onClick={() => this.fetchUser()}>
+          Fetch user
+        </Button>
+        {error && <h1>{error.message}</h1>}
+        {isLoading && <CircularProgress />}
+        {!isLoading &&
+          <React.Fragment>
+            <h1>Seed: {fetchedData.info.seed}</h1>
+            {fetchedData.results.map((result, index) => (
+              <h1 key={index}>
+                {result.gender}
+              </h1>
+            ))}
+          </React.Fragment>
+        }
+      </Container>
+    )
+  }
 }
 
 export default App;
